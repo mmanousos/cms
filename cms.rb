1e65c1df306843f0bb01d7fc8b2afa36daaec134
Melissa Manousos <mmanousos@gmail.com>
2019-02-04 14:54:06 -0800

    Adds view of individual documents

--- cms.rb
+++ cms.rb
@@ -9,6 +9,20 @@ configure do
   set :erb, :escape_html => true
 end
 
-get '/' do
-  "Getting started."
+before do
+  @root = File.expand_path("..", __FILE__)
+  @files = Dir.glob(@root + "/data/*").map do |path|
+    File.basename(path)
+  end
+end
+
+get "/" do
+  erb :index
+end
+
+# display document
+get '/:file_name' do
+  file_path = @root + '/data/' + params[:file_name]
+  headers['Content-Type'] = 'text/plain'
+  @doc = File.read(file_path)
 end
